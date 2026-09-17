f8acad8dd772335c22a46a236340a721e4e77061
Julia-lab-rivera <julian.rivera01@usa.edu.co>
2026-09-17 12:28:01 -0500

    Add files via upload

---
+++ objetos_perdidos/src/back/model/EstadoObjeto.java
@@ -0,0 +1,11 @@
+package back.model;
+
+/**
+ * Estados posibles del ciclo de vida de un objeto dentro del sistema.
+ */
+public enum EstadoObjeto {
+    REGISTRADO,
+    PERDIDO,
+    ENCONTRADO,
+    DEVUELTO
+}
